158d2a9f72bfc33f4ca2fe6da3330206b4df3173
fajards0 <dmasivdiabloo@gmail.com>
2026-04-22 20:32:08 +0700

    Use artisan serve for Render runtime

--- docker/start.sh
+++ docker/start.sh
@@ -5,10 +5,6 @@ cd /var/www/html
 
 PORT="${PORT:-8000}"
 
-sed -i "s/Listen 80/Listen ${PORT}/" /etc/apache2/ports.conf
-sed -i "s/\*:80/*:${PORT}/" /etc/apache2/sites-available/000-default.conf
-sed -i "s/\${PORT}/${PORT}/g" /etc/apache2/sites-available/000-default.conf
-
 php artisan config:clear || true
 php artisan cache:clear || true
 php artisan view:clear || true
@@ -25,4 +21,4 @@ php artisan config:cache
 php artisan route:cache
 php artisan view:cache
 
-apache2-foreground
+exec php artisan serve --host=0.0.0.0 --port="${PORT}"
